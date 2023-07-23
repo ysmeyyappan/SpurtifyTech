9411a8d32dac5da992b94d557aaabf14a998e349
Meyyappan <ysmeyyappan@gmail.com>
2023-07-23 20:54:19 +0530

    Add existing project files to Git

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div>
+      <link rel="stylesheet" href="https://unpkg.com/tachyons@4/css/tachyons.min.css"></link>
+      <div  style={{"background-image": "linear-gradient(120deg, #ddd6f3 0%, #faaca8 100%)" , height:"1800px"}}>
+         <div>
+            Hello Spurtify Tech
+         </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
